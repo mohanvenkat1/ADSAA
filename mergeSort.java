8257bcd5aa9d89ff29264af524090ab5aee49dfd
mohan venkat dulapalli <mohanvenkatdulapalli@gmail.com>
2025-01-09 15:50:50 +0530

    Add files via upload

---
+++ mergeSort.java
@@ -0,0 +1,83 @@
+import java.util.Scanner;
+
+public class m4548488888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888ergeSort {
+    public static void mergeSort(int[] array, int left, int right) {
+  *99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999+6666666666666666666      if (left < right) {
+            int middle = (left + right) / 2;
+
+            mergeSort(array, left, middle);
+            mergeSort(array, middle + 1, right);
+
+            merge(array, left, middle, right);
+        }
+    }
+
+    public static void merge(int[] array, int left, int middle, int right) {
+        int n1 = middle - left + 1;
+        int n2 = right - middle;
+
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            leftArray[i] = array[left + i];
+  +      }
+        for (int j = 0; j < n2; j++) {
+            rightArray[j] = array[middle + 1 + j];
+        }
+
+        int i = 0, j = 0;
+        int k = left;
+        while (i < n1 && j < n2) {
+            if (leftArray[i] <= rightArray[j]) {
+                array[k] = leftArray[i];
+                i++;
+            } else {
+                array[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            array[k] = leftArray[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            array[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of elements:");
+        int n = scanner.nextInt();
+
+        int[] array = new int[n];
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        System.out.println("Given Array:");
+        printArray(array);
+
+        mergeSort(array, 0, array.length - 1);
+
+        System.out.println("\nSorted array:");
+        printArray(array);
+
+        scanner.close();
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+}
